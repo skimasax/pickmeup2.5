bc4eeda71e3287ca899ed98324c974666934b6dd
Abimbola <youngskima@gmail.com>
2022-05-20 13:30:32 +0100

    Api AUTHENTICATION Updated

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,15 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/signup',[UserController::class, 'signup'])->name('signup');
-Route::post('/getusers/{id}',[UserController::class, 'getUsers'])->name('getusers');
-Route::post('/update/{id}',[UserController::class, 'updateProfile'])->name('update profile');
+//public routes
+Route::get('/signup',[UserController::class, 'signup'])->name('signup');
 Route::post('/login',[UserController::class, 'login'])->name('login');
 
-
+//protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/getusers/{id}',[UserController::class, 'getUsers'])->name('getusers');
+    Route::post('/update/{id}',[UserController::class, 'updateProfile'])->name('update profile');
+});
 
 
 
